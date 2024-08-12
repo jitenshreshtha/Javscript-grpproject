b46ef13c8359fb8dbd4b6c4ab8956da96f8b6a1d
G10 Shreshtha <g10sth@gmail.com>
2024-08-12 16:55:16 -0400

    all files added

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,41 +1,67 @@
-const $ = selector => document.querySelector(selector);
+'use strict';
+$(()=>{
+  let currentIndex = 0;
+  function showNextSlide(){
+    const slides = $('.slide');
+    slides.eq(currentIndex).removeClass('active');
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides.eq(currentIndex).addClass('active');
+  }
+  setInterval(showNextSlide,3000);
+  $('.slide').first().addClass('active');
 
-document.addEventListener('DOMContentLoaded',()=>{
+
+
+
   $('#searchBar').focus();
-  const productList = $('.productList');
-  let products = [];
+
+  let productList = $('.productList');
+  let products =[];
 
   fetch('../Javascript/Data.JSON')
     .then((res)=>res.json())
     .then((data)=>{
-      products = data;
+      products =data;
       renderProducts(products);
     })
-    $('#searchBtn').addEventListener('click',()=>{
-      console.log('clicked')
-      const searchData = $('#searchBar').value.trim().toLowerCase();
-      const filteredProducts = products.filter((product)=>{
-        return product.title.toLowerCase().includes(searchData);
-      });
-      renderProducts(filteredProducts);
+
+
+
+    $('#searchBtn').on('click',()=>{
+      console.log('clicked');
+      const searchedData = $('#searchBar').val().trim().toLowerCase();
+      const filteredProducts = products.reduce((acc,product) =>{
+        const filteredProductList = product.productList.filter((item)=>{
+          return (
+            item.title.toLowerCase().includes(searchedData) ||
+            item.description.toLowerCase().includes(searchedData) ||
+            item.tags.some((tag) => tag.toLowerCase().includes(searchedData))
+          )
+        });
+        return acc.concat(filteredProductList);
+      },[]);
+      if(filteredProducts.length > 0){
+        renderProducts(filteredProducts);
+      }
+      else{
+        $('#product-List').html('<p>No results found.</p>')
+      }
+      
     })
 
 
-    function renderProducts(products){
-      productList.innerHTML = '';
-      products.forEach((product)=>{
-        const productDiv = document.createElement('div');
-        productDiv.classList.add('product');
-
-        productDiv.innerHTML = `
+  function renderProducts(products){
+    productList.empty();
+    $.each(products, (index,product)=>{
+      const productDiv = $('<div>').addClass('product');
+      productDiv.html(`
         <a href='./product.html?productName=${product.title}'>
         <img src='${product.image}' alt='${product.title}'>
         <h2>${product.title}</h2>
         </a>
-        `
-
-        productList.appendChild(productDiv);
-      })
-    }
+        `);
+        productList.append(productDiv);
+    });
+  }
 
 })
